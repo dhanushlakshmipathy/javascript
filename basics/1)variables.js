ddd3b97ae957fa597b75eeb3985175e0db6ab3d7
dhanushlakshmipathy <lakshmipathydhanush@gmail.com>
2025-09-02 10:32:32 +0530

    Dataypes

---
+++ basics/1)variables.js
@@ -0,0 +1,10 @@
+const accountId=12345;//declaration and initialization of variable 
+let accountEmail="lakshmipathydhanush@gmail.com"
+var accountPassword="Dhanush@123"
+accountCity="Chennai"
+// prefer not to use var, because of issue in block scope and functional scope
+//accountId =2 //not allowed 
+accountId=2
+accountEmail="Smthg@gmail.com";
+console.log(accountId)
+
---
+++ basics/2_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all js code as newer version.
+
+// alert("hello"); //we can use alert, prompt and confirm only in the browser not in nodejs
+//we are using node js not browser. 
+
+// console.log(3+3); console.log("dhanush"); //not good readability
+console.log(3+3);
+console.log("dhanush");//good readability
+
+let name="Dhanush"; //string
+let age=24; //number
+let isApproved=true; //boolean
+let firstName=undefined; //undefined
+let selectedColor=null; //null 
+
+//number ==> 2 to  power 53 
+//bigint
+//string==" "
+//boolean==true/false
+//undefined
+//null==empty value
+//symbol =>unique 
+
+//object 
+
+console.log(typeof name);
+console.log(typeof age); 

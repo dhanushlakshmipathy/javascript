c0d51783b389b0a36a10cb239f6e4083de79d256
dhanushlakshmipathy <lakshmipathydhanush@gmail.com>
2025-09-02 11:02:34 +0530

    8th done

---
+++ basics/3.ConversionOperations.js
@@ -0,0 +1,43 @@
+let score="33abc"; 
+
+console.log(typeof(score));  
+
+let valueinNumber=Number(score); //explicit conversion 
+console.log(typeof valueinNumber); 
+
+//"33" ==>33 
+//"33abc" ==>NaN
+//true ==>1
+//false ==>0
+//null ==>0 
+
+// ***************************Operations *************************  
+
+let value=4;
+let negValue=-value; 
+console.log(negValue);
+
+// console.log(2+3); //5
+// console.log("2"+"3");
+// console.log(2-3);
+// console.log(3*4);
+// console.log(10/2);
+// console.log(10%3); //1
+// console.log(2**3); //2^3=8
+// +,-,*,/,% ,** 
+
+let str1="Hollow";
+let str2=" Knight";
+
+let str3=str1+str2;
+console.log(str3);
+
+console.log("2"+3); //23
+console.log(2+"3"); //23 
+console.log("2"+"3");
+console.log("2"+1+1); //211 
+console.log(2+1+"1"); //31 
+let gamecount =100;
+
+gamecount++; //gamecount=gamecount+1 
+
---
+++ basics/4.comparision.js
@@ -0,0 +1,20 @@
+// console.log(4>1);
+// console.log(4<1);
+// console.log(4>=1);
+// console.log(4<=1);
+// console.log(4==1);
+// console.log(4!=1);
+
+console.log("2">1); 
+
+console.log(null==0);
+console.log(null>=0);
+console.log(null>0);
+console.log(null<=0);
+console.log(null<0); 
+
+//dont do these type of comparisions 
+//===
+console.log(null===0);
+console.log("2"===2);
+console.log("2"==2);

5b5b2468c546438ef1c7d4c9afc08ea8e9695180
dhanushlakshmipathy <lakshmipathydhanush@gmail.com>
2025-09-03 11:03:49 +0530

    chumma

--- basics/4.comparision.js
+++ basics/4.comparision.js
@@ -5,6 +5,7 @@
 // console.log(4==1);
 // console.log(4!=1);
 
+
 console.log("2">1); 
 
 console.log(null==0);
@@ -18,3 +19,42 @@ console.log(null<0);
 console.log(null===0);
 console.log("2"===2);
 console.log("2"==2);
+//primitive 
+//based on how they r stored in the memory and accessed. 
+const score=100
+const scoreValue=100.3
+const isloggedIn=false;
+const outsideTemp=null;
+//7 types: String , Number, Boolean, null, undefined, Symbol, BigInt 
+//non-primitive 
+//array, object, function 
+
+const heros=["shaktiman","nagraj","doga"];
+let myObj={
+    name:"Dhanush",
+    age:24
+} 
+// *************************************************************************
+
+//memory storage type///
+
+//Stack(Primitive), Heap(Non-Primitive)
+//whatever primitive type we create it will be stored in stack memory 
+let myYoutubename="Dhanush"; //stored in stack 
+
+let anotherName=myYoutubename; //copy of myYoutubename is created and stored in anotherName 
+anotherName="Ravi"; //anotherName is updated but myYoutubename is not updated 
+
+console.log(anotherName); //Dhanush 
+console.log(myYoutubename); //Ravi 
+
+
+let user={
+    email: "user@gmal.com",
+    upi: "user@okicici" 
+}
+
+let userTwo=user; //copy of user is created and stored in userTwo 
+
+userTwo.email="dhanush@gmail.com"; 
+console.log(userTwo.email); //
---
+++ basics/5.Strings.js
@@ -0,0 +1,24 @@
+const name="Dhanush"; 
+const repoCount=100; 
+
+// console.log(name + "has repo count of "+repoCount); //Dhanush100     
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); //Hello ${name} 
+
+
+const gameName= new String('PUBG'); //String object 
+
+console.log(gameName[0]);
+console.log(gameName.__proto__); //all the methods of string 
+
+console.log(gameName.length); 
+console.log(gameName.toUpperCase());
+console.log(gameName.toLowerCase());
+console.log(gameName.charAt(2)); 
+console.log(gameName.indexOf('G'));
+
+const newString=gameName.substring(0,3);
+console.log(newString);
+
+const anotherString=gameName.slice(0,3); 
+console.log(anotherString); 
